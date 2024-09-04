5d50c5bb12587b7e5bc4df40d542dc02ac5f242a
omarawadalla2 <omarawadalla4@gmail.com>
2024-09-03 21:36:19 -0400

    Add files via upload

---
+++ lab-1-Omar-Awadalla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+  int age;
+  float height;
+  char grade;
+
+  printf("Enter your age: ");
+  scanf("%d",&age);
+  
+  printf("Enter your height: ");
+  scanf("%f", &height);
+
+  printf("Enter your grade: ");
+  scanf(" %c", &grade);
+
+  printf("You entered - Age: %d, Height: %.1f, Grade: %c", age, height, grade);
+  
+  return 0;
+}
